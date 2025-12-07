3321b92625d30dac2025d7f3b2d396e26e2a6186
joh-brunner <joh.brunner@fau.de>
2025-12-07 11:46:14 +0100

    cf compatible templates

--- create_nc.py
+++ create_nc.py
@@ -1,73 +1,111 @@
-# Create nc-axes with dimensions and coordinates (yearly time dimension with bounds)
-# Make either glacier-region axis or glacier-id axis depending if you submit regional or glacier specific results
+# What unit should be used for time? CF says "days since epoch", but years since epoch could also be an option
 
-# What unit should be used for time? CF says days since epoch
-# Years since epoch could also be an option
+# toDo: Add sample data from csv-table as variables
 
 import xarray as xr
-
 import numpy as np
-import xarray as xr
 
-# 1) Declare axis names (these are the names of dimensions you'll use)
-TIME_DIM = "time_year"  # yearly time axis
-NB_BOUNDS = "nv"  # small secondary dim for bounds (size 2)
-GLACIER_DIM = "glacier"
-
-num_of_glaciers = 5  # or regions
-
-# Make the timeseries one timestep longer for the bounds variables
+# Start the timeseries one timestep earlier for the bounds variables
+# This could be improved by automatically loading the the timestep (monthly and annual) prior to the epoch date
 epoch_date = "2000-01-01"
-start_date = "1999-01-01"
-end_date = "2101-01-02"  # set to 2301 if necessary
-
-# 2) Prepare coordinate arrays / labels you want to use
-
-# TIME
-timestep = "annual"  # either "annual" or "monthly"
-
-##  Create the numpy timeseries
-epoch = np.datetime64(epoch_date, "D")
-start = np.datetime64(start_date, "D")
-end = np.datetime64(end_date, "D")
-np_days = np.arange(start, end, dtype="datetime64[D]")
-
-if timestep == "annual":
-    # ANNUAL
-    # Extract first days of each year
-    dates = np_days[np_days == np_days.astype("datetime64[Y]")]
-
-elif timestep == "monthly":
-    # MONTHLY
-    # Extract first days of each month
-    dates = np_days[np_days == np_days.astype("datetime64[M]")]
-
-# convert to days-since-epoch (integers)
-days_since_epoch = (dates - epoch).astype("timedelta64[D]").astype(np.int64)
-
-## ADD bounds for time
-# Create bounds (time_bounds)
-bounds = np.empty((len(days_since_epoch) - 2, 2), dtype=np.int64)  # One less row
-bounds[:, 0] = days_since_epoch[0:-2]
-bounds[:, 1] = days_since_epoch[1:-1]
+start_date_month = "1999-12-01"
+start_date_year = "1999-01-01"
+end_date = "2100-01-02"  # set to 2301 if necessary,
 
 
-# Example
-n_time = 101
-mass = np.linspace(10, 5, n_time)
-mass_loss = np.diff(mass)
-mass_loss = np.concatenate(([0], mass_loss))
+def main():
+    # Create an empty template nc with correct axes
+    ds = create_template_nc()
+    ds.to_netcdf("template.nc")
 
-ds = xr.Dataset({"mass": (["time"], mass), "mass_loss": (["time"], mass_loss), "time_bounds": (["time", "nbounds"], bounds)}, coords={"time": days_since_epoch[1:-1]})
-
-ds["time"].attrs["units"] = "days since 1970-01-01"
-ds["time"].attrs["bounds"] = "time_bounds"
+    # Add some sample data to the nc
+    filled_ds = add_sample_data(ds)
+    filled_ds.to_netcdf("template_with_data.nc")
 
 
-ds["mass_loss"].attrs["cell_methods"] = "time: sum"
-ds["mass"].attrs["cell_methods"] = "time: point"
+def create_template_nc():
+    # TIME
+    ##  Create the numpy timeseries
+    epoch = np.datetime64(epoch_date, "D")
+    end = np.datetime64(end_date, "D")
 
-# Save to NetCDF file
-ds.to_netcdf("example.nc")
+    annual_time, annual_bounds = get_time_and_bounds(start_date_year, end, epoch, "datetime64[Y]")
+    monthly_time, monthly_bounds = get_time_and_bounds(start_date_month, end, epoch, "datetime64[M]")
 
-print(ds)
+    # TEMPLATE NC
+    ds = xr.Dataset(
+        coords={
+            "annual_time": annual_time,
+            "monthly_time": monthly_time,
+            "nbounds": [0, 1],
+            "glacier_id": [""],
+        },
+        data_vars={
+            "annual_time_bounds": (["annual_time", "nbounds"], annual_bounds),
+            "monthly_time_bounds": (["monthly_time", "nbounds"], monthly_bounds),
+        },
+    )
+
+    set_time_attributes(ds, "annual", epoch)
+    set_time_attributes(ds, "monthly", epoch)
+
+    # Save to NetCDF file
+    return ds
+
+
+def add_sample_data(ds):
+    # Example data
+    n_time = 101
+    annual_mass = np.linspace(10, 5, n_time)
+
+    n_time_month = 1201
+    mass_month = np.linspace(10, 5, n_time_month)
+    mass_change_monthly = np.diff(mass_month)
+    mass_change_monthly = np.concatenate(([0], mass_change_monthly))
+
+    # Add annual_mass and and mass_change_monthly to nc-file
+    ds = xr.open_dataset("template.nc")
+
+    # Add a new glacier to the dataset
+    my_glacier_ids = np.array(["RGI7_123456"])
+    ds.coords["glacier_id"] = my_glacier_ids
+
+    # Find the index of the target glacier_id in the 'glacier_id' coordinate
+    glacier_idx = np.where(ds.coords["glacier_id"].values == "RGI7_123456")[0][0]
+
+    # Create a new variable
+    ds["mass"] = (["glacier_id", "annual_time"], np.zeros((len(ds.coords["glacier_id"]), len(ds.coords["annual_time"]))))
+    # Copy the data into the ds
+    ds["mass"].values[glacier_idx, :] = annual_mass
+    # Set its attribute
+    ds["mass"].attrs["cell_methods"] = "time: point"
+
+    # Create a second variable (code duplication)
+    ds["mass_change"] = (["glacier_id", "monthly_time"], np.zeros((len(ds.coords["glacier_id"]), len(ds.coords["monthly_time"]))))
+    ds["mass_change"].values[glacier_idx, :] = mass_change_monthly
+    ds["mass_change"].attrs["cell_methods"] = "time: sum"
+
+    return ds
+
+
+def get_time_and_bounds(start_date, end, epoch, datetime_type="datetime64[Y]"):
+    np_days = np.arange(np.datetime64(start_date, "D"), end, dtype="datetime64[D]")
+    dates = np_days[np_days == np_days.astype(datetime_type)]
+
+    days_since_epoch = (dates - epoch).astype("timedelta64[D]").astype(np.int64)
+
+    bounds = np.empty((len(days_since_epoch) - 1, 2), dtype=np.int64)
+    bounds[:, 0] = days_since_epoch[0:-1]
+    bounds[:, 1] = days_since_epoch[1:]
+
+    return days_since_epoch[1:], bounds
+
+
+def set_time_attributes(ds, resolution, epoch):
+    var = resolution + "_time"
+    ds[var].attrs["long_name"] = resolution + " time (days since " + str(epoch) + ")"
+    ds[var].attrs["units"] = "days since " + str(epoch)
+    ds[var].attrs["bounds"] = resolution + "_time_bounds"
+
+
+main()
